d2a31d1ebe24c993fdecae7e38bc476e5f69d53a
freQuensy <mametevalex@gmail.com>
2020-06-13 11:52:11 +0300

    Стабильная версия с базой данных. Неадекватно работает комиссия и не сделан выбор прав значений из БД. Уведомления / интернет не работают.

--- app/src/main/java/com/currency_converter/ui/home/HomeFragment.kt
+++ app/src/main/java/com/currency_converter/ui/home/HomeFragment.kt
@@ -14,9 +14,12 @@ import com.currency_converter.R
 import kotlinx.android.synthetic.main.fragment_home.*
 import android.content.ClipboardManager
 import android.content.Context
+import android.text.Editable
 import android.text.TextWatcher
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.core.content.ContextCompat.getSystemService
-
+import org.w3c.dom.Text
 
 
 fun show_msg(text:String, long:Boolean = false, show_log:Boolean = true, activity: Activity?){
@@ -36,7 +39,6 @@ fun swap(b:Boolean):Boolean{
 
 
 
-
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
@@ -49,14 +51,52 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        //val textView: TextView = root.findViewById(R.id.text_home)
-        //homeViewModel.text.observe(viewLifecycleOwner, Observer {
-        //    textView.text = it
-        //})
+
+
+
+
+
+
         return root
     }
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var use_database_values : Boolean = true
+        var DB_value_1_cost :Float = 75.00.toFloat()
+        var DB_value_1_index:Int = 1
+        var DB_value_2_cost :Float = 75.00.toFloat()
+        var DB_value_2_index:Int = 5
+
+        //Creating values arrays
+        val USD : Float = 75.00.toFloat()
+        val UAH : Float = 2.62.toFloat()
+        val GBD : Float = 87.93.toFloat()
+        val EUR : Float = 79.01.toFloat()
+        val BIT : Float = 79000.1.toFloat()
+        val RUB : Float = 1.0.toFloat()
+        val costs : Array<Float>   = arrayOf( USD,   UAH,   GBD,   EUR,   BIT,   RUB)
+        val values : Array<String> = arrayOf("USD", "UAH", "GBD", "EUR", "BIT", "RUB")
+
+        try {
+            convert_from_spinner.adapter =
+                ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, values)
+            convert_to_spinner.adapter =
+                ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, values)
+            convert_to_spinner.setSelection(DB_value_2_index)
+        }
+        catch(e : Exception){Log.d("exception_adapter", "Exception on creationing adapter $e")}
 
         var use_comission: Int = 0
         var comis_percent: Int = 2
@@ -64,13 +104,33 @@ class HomeFragment : Fragment() {
 
         var comis_total: Float = 0.toFloat()
 
-        super.onViewCreated(view, savedInstanceState)
+
 
         result_editText.setEnabled(false);
         result_editText.setCursorVisible(false);
         Log.d("tag", result_editText.text.toString())
 
         //TODO Сделать форматирование что бы выводилоось только 2 знака после запятой в поле результата
+        //TODO Исправить комиссию банка она не работае ткоректно
+
+
+
+        cost_editText.addTextChangedListener(object  : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("editText", "cost_editText 3")
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("editText", "cost_editText 2")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("editText", "cost_editText changed")
+                try{
+                use_database_values = false}
+                catch (e : Exception){Log.d("Exception", "Use database exc. ${e}")}
+            }
+        })
 
         do_convert_button.setOnClickListener {
             Log.d("button", "Convert_button_pressed")
@@ -117,31 +177,52 @@ class HomeFragment : Fragment() {
 
         do_replace_button.setOnClickListener {
             Log.d("button", "replace_button_pressed")
-            try {
-                var cost: Float = cost_editText.text.toString().toFloat()
-                if (cost.toDouble() == 0.0) {
-                    Log.d("values", "cost could't be ZERO, ${cost}")
-                    val text = "Стоимость не может быть 0."
+
+            if (use_database_values) {
+                var tmp: Int = convert_from_spinner.selectedItemId.toInt()
+                Log.d("values", "tmp = ${tmp}")
+                convert_from_spinner.setSelection(convert_to_spinner.selectedItemId.toInt())
+                Log.d(
+                    "values",
+                    "convert_to_spinner.selectedItemId = ${convert_to_spinner.selectedItemId.toInt()}"
+                )
+                try {
+                    convert_to_spinner.setSelection(tmp)
+                } catch (e: Exception) {
+                    Log.d("Exception", "Exception on spinner changing $e")
+                }
+//                finally {
+//                    Log.d("Exception", "Smth going wrong finaly in spinner")
+//                }
+            }
+            else
+            {
+                try {
+                    var cost: Float = cost_editText.text.toString().toFloat()
+                    if (cost.toDouble() == 0.0) {
+                        Log.d("values", "cost could't be ZERO, ${cost}")
+                        val text = "Стоимость не может быть 0."
+                        val activity: Activity? = activity
+                        show_msg(text, activity = activity)
+                    } else {
+                        cost_editText.setText((1 / cost).toString())
+                    }
+                } catch (e: Exception) {
+                    Log.d("values", "enter cost please")
+                    val text = "Введите стоимость валюты."
                     val activity: Activity? = activity
                     show_msg(text, activity = activity)
-                } else {
-                    cost_editText.setText((1 / cost).toString())
                 }
-            } catch (e: Exception) {
-                Log.d("values", "enter cost please")
-                val text = "Введите стоимость валюты."
-                val activity: Activity? = activity
-                show_msg(text, activity = activity)
             }
-        }
 
-        comission_use_swich.setOnCheckedChangeListener() { _, isChecked ->
-            if (isChecked) {
-                Log.d("switch", "Use comission")
-                use_comission = 1
-            } else {
-                Log.d("switch", "Don't use comission")
-                use_comission = 0
+            comission_use_swich.setOnCheckedChangeListener() { _, isChecked ->
+                if (isChecked) {
+                    Log.d("switch", "Use comission")
+                    use_comission = 1
+                } else {
+                    Log.d("switch", "Don't use comission")
+                    use_comission = 0
+                }
             }
         }
 
